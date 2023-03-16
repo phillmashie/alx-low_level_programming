7c3ae3e7e6459f83886f96f405b9cc5a432c4b31
phillmashie <phillmashie@gmail.com>
2023-03-16 21:36:00 +0000

    Use the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program which print a line using the command puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
