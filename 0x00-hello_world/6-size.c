9c3cc84adabf56cd1030f5e020b78386470601e1
phillmashie <phillmashie@gmail.com>
2023-03-19 19:03:19 +0000

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(L));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+return(0);
 }
