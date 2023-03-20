f92d7ef66836d1b6fa1f141b4c94b31c7d6999cb
phillmashie <phillmashie@gmail.com>
2023-03-20 07:54:33 +0000

    The compiled file output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
-	printf("Size of an int: %uz byte(s)\n", sizeof(i));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(L));
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
