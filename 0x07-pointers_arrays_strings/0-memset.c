8bd3d10d2d52b03d4f9b151240b3db4243cf236c
phillmashie <phillmashie@gmail.com>
2023-04-04 20:12:30 +0000

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
-* _memset - fill memory .
-* @s: pointer
-* @b: hexadecimal number
-* @n: n bytes
-* Return: pointer
-*/
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned char *u = (unsigned char *) s;
+	unsigned int i = 0;
 
-	while (n-- > 0x00)
+	while (i < n)
 	{
-		*u++ = b;
+		*(s + i) = b;
+		i++;
 	}
 	return (s);
 }
