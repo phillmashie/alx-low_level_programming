d7650bc40ec28f17fedd2981fbfecfc559c70fe4
phillmashie <phillmashie@gmail.com>
2023-04-04 20:09:30 +0000

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,26 +1,19 @@
-/*
- * File: 0-memset.c
- * Auth: Phillip Mashingaidze
- */
-
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
- */
-void *_memset(void *s, int c, size_t n)
+* _memset - fill memory .
+* @s: pointer
+* @b: hexadecimal number
+* @n: n bytes
+* Return: pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned char *u = (unsigned char *) s;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	while (n-- > 0x00)
+	{
+		*u++ = b;
+	}
+	return (s);
 }
