8c9219e876cc86b6b4aea416db6c1b8585c0dfd3
phillmashie <phillmashie@gmail.com>
2023-04-06 17:33:39 +0000

    advanced tasks on recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,17 +12,11 @@
  * Return: If n > 0 - the factorial of n.
  *         If n < 0 - 1 to indicate an error.
  */
-int factorial(int n)
+int _strlen_recursion(char *s)
 {
-	int result = n;
-
-	if (n < 0)
-		return (-1);
-
-	else if (n >= 0 && n <= 1)
-		return (1);
-
-	result *= factorial(n - 1);
-
-	return (result);
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
